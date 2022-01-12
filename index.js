e2f12af3047883325d5c143adacc7b574d765124
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-01-12 22:58:09 +0000

    api call data is now cached for an hour

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
 const dotenv = require('dotenv');
+const Cache = require('./cache');
 dotenv.config();
 const TOKEN = process.env.TOKEN;
+const cache = new Cache(3600);
 
 app.get('/pop', async (request, response) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
-  const apiCall = await axios.get(`https://api.discogs.com/database/search?&format=album&style=pop&country=US&per_page=25&year=2021&token=${TOKEN}`);
-  const data = apiCall.data;
+  const requestUrl = `https://api.discogs.com/database/search?&format=album&style=pop&type=release&country=US&per_page=25&year=2021&token=${TOKEN}`;
+  const data = await cache.get(requestUrl);
   response.json(data);
 });
 
 app.get('/hiphop', async (request, response) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
-  const apiCall = await axios.get(`https://api.discogs.com/database/search?&format=album&style=hip+hop&country=US&per_page=25&year=2021&token=${TOKEN}`);
-  const data = apiCall.data;
+  const requestUrl = `https://api.discogs.com/database/search?&format=album&style=hip+hop&type=release&country=US&per_page=25&year=2021&token=${TOKEN}`;
+  const data = await cache.get(requestUrl);
   response.json(data);
 });
 
 app.get('/jazz', async (request, response) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
-  const apiCall = await axios.get(`https://api.discogs.com/database/search?&format=album&style=jazz&country=US&per_page=25&year=2021&token=${TOKEN}`);
-  const data = apiCall.data;
+  const requestUrl = `https://api.discogs.com/database/search?&format=album&style=jazz&type=release&country=US&per_page=25&year=2021&token=${TOKEN}`;
+  const data = await cache.get(requestUrl);
+  response.json(data);
+});
+
+app.get('/:type/:id', async (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  const requestUrl = `https://api.discogs.com/${request.params.type}/${request.params.id}`;
+  const data = await cache.get(requestUrl);
   response.json(data);
 });
 
