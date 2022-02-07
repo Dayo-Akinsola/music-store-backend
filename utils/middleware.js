5ff2c5494a3daaafcb8ad38b943a4d4f25ff9f6d
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-07 21:46:54 +0000

    added check for jsonwebtoken errors

--- utils/middleware.js
+++ utils/middleware.js
@@ -3,15 +3,16 @@ const unknownEndpoint =  (req, res) => {
 }
 
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
-
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id'});
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message});
-  } else{
-    next(error);
+  } else if (error.name === 'JsonWebTokenError') {
+    return res.status(401).json({error: 'invalid token'});
   }
+
+  console.error(error.message);
+  next(error);
 }
 
 module.exports = {
