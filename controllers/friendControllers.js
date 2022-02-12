8aab4693e971bea03303296e7dec1e4ff1ec97bf
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-12 21:02:12 +0000

    added controllers and routes for responding to a friend request

--- controllers/friendControllers.js
+++ controllers/friendControllers.js
@@ -2,26 +2,62 @@ const { getToken, getLoggedInUser } = require('../helpers/serviceHelpers');
 const User = require('../models/user');
 
 const FriendControllers = (() => {
-  const getUserFriends = async (req, res, next) => {
+
+  const _logInUser = async (req) => {
     const token = getToken(req);
     const loggedInUser = await getLoggedInUser(token);
-  
-    if (loggedInUser) {
-      res.json(loggedInUser.friends)
+    return loggedInUser;
+  }
+
+  const _requestToSelfCheck = (loggedInUser, body) => {
+    if (loggedInUser.name !== body.name || loggedInUser.username !== body.username) {
+      return false;
     }
+    return true;
+  }
+
+  const _duplicateRequestFound = (loggedInUser, body) => {
+    const { sentRequests, receivedRequests, friends } = loggedInUser;
+
+    const duplicateFound = (userList) => {
+      const filteredUserlist  = userList.filter((user) => user.name === body.name && user.username === body.username);
+      if (filteredUserlist.length === 0) {
+        return false;
+      }
+      return true;
+    }
+
+    if (duplicateFound(sentRequests) || duplicateFound(receivedRequests) || duplicateFound(friends)) {
+      return true;
+    }
+    return false;
+  }
   
+  const getUserList = async (req, res, next, list) => {
+    const loggedInUser = await _logInUser(req);
+
+    if (loggedInUser) {
+      res.json(loggedInUser[list])
+    }
     next();
   }
 
   const sendFriendRequest = async (req, res, next) => {
+    const loggedInUser = await _logInUser(req);
     const { body } = req;
-    const token = getToken(req);
-    const loggedInUser = await getLoggedInUser(token);
   
     if (loggedInUser) {
+      if (_requestToSelfCheck(loggedInUser, body)) {
+        return res.status(400).json({ error: 'You cannot send a request to yourself.'});
+      }
+
       const targetUser = await User.findOne({ name: body.name, username: body.username });
       if (!targetUser) {
         return res.status(400).json({ error: 'This user does not exists'});
+      }
+
+      if (_duplicateRequestFound(loggedInUser, body)) {
+        return res.status(400).json({ errror: 'You have already sent, received or are friends with this user.'})
       }
       loggedInUser.sentRequests.push(targetUser);
       targetUser.receivedRequests.push(loggedInUser);
@@ -32,9 +68,49 @@ const FriendControllers = (() => {
     next();
   }
 
+  const respondToFriendRequest = async (req, res, next) => {
+    const loggedInUser = await _logInUser(req);
+    const { body } = req;
+
+    if (loggedInUser) {
+      const receiver = await User.findOne({ name: loggedInUser.name, username: loggedInUser.username})
+        .populate({ path: 'sentRequests receivedRequests friends', select:'name username'});
+      const sender = await User.findOne({ name: body.name, username: body.username})
+        .populate({ path: 'sentRequests receivedRequests friends', select:'name username'});
+      const { receivedRequests, friends } = receiver;
+
+      if (body.accept) {
+        friends.push(sender);
+        sender.friends.push(receiver);
+      }
+
+      receivedRequests.forEach((request, index) => {
+        if (request._id.toString() === sender._id.toString()) {
+          receivedRequests.splice(index, 1);
+        }
+      });
+
+      sender.sentRequests.forEach((request, index) => {
+        if (request._id.toString() === receiver._id.toString()) {
+          sender.sentRequests.splice(index, 1);
+        }
+      });
+
+      await receiver.save();
+      await sender.save();
+      res.status(200).json({ message: `You are now friends with ${sender.name}`});
+    }
+    next();
+  }
+
+  const getFriendsWishlist = async (req, res, next) => {
+
+  }
+
   return {
-    getUserFriends,
+    getUserList,
     sendFriendRequest,
+    respondToFriendRequest,
   }
 })();
 
--- routes/friends.js
+++ routes/friends.js
@@ -1,8 +1,14 @@
 const friendsRouter = require('express').Router();
 const FriendControllers = require('../controllers/friendControllers');
 
-friendsRouter.get('/request', (req, res, next) => FriendControllers.getUserFriends(req, res, next));
+friendsRouter.get('/friendlist', (req, res, next) => FriendControllers.getUserList(req, res, next, 'friends'));
+
+friendsRouter.get('/request/sent', (req, res, next) => FriendControllers.getUserList(req, res, next, 'sentRequests'));
+
+friendsRouter.get('/request/received', (req, res, next) => FriendControllers.getUserList(req, res, next, 'receivedRequests'));
 
 friendsRouter.post('/request', (req, res, next) =>  FriendControllers.sendFriendRequest(req, res, next));
+
+friendsRouter.post('/request/response', (req, res, next) => FriendControllers.respondToFriendRequest(req, res, next));
 
 module.exports = friendsRouter;
