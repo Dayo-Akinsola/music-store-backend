b6ebe1ba168399b8241b5fc360d9b1d693d1f8b0
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-02-17 23:08:18 +0000

    adding route to get all a users orders

--- routes/orders.js
+++ routes/orders.js
@@ -3,4 +3,6 @@ const OrderControllers = require('../controllers/orderControllers');
 
 ordersRouter.post('/', (req, res) => OrderControllers.addOrder(req, res));
 
+ordersRouter.get('/', (req, res) => OrderControllers.getOrders(req, res));
+
 module.exports = ordersRouter;
