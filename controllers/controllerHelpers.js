783b5bf5f3e69f54856dd42fc7509af269855158
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-03-09 20:29:53 +0000

    added helper method to match the id of an album sent fromt he frontend to the albums sent from the discogs api calls

--- controllers/controllerHelpers.js
+++ controllers/controllerHelpers.js
@@ -1,4 +1,7 @@
 const { getToken, getLoggedInUser } = require('../helpers/serviceHelpers');
+const Cache = require('../cache');
+const cache = new Cache(3600);
+const TOKEN = process.env.TOKEN;
 
 const logInUser = async (req, next) => {
   const token = getToken(req);
@@ -14,5 +17,22 @@ const isUserLoggedIn = (req) => {
   return false;
 }
 
+const findMatchingAlbum = async (albumId) => {
+  const headers = {
+    withCrendentials: true,
+  }
+  const popRequestUrl = `https://api.discogs.com/database/search?&format=album&style=pop&type=release&country=US&per_page=75&year=2021&token=${TOKEN}`;
+  const hipHopRequestUrl = `https://api.discogs.com/database/search?&format=album&style=hip+hop&type=release&country=US&per_page=75&year=2021&token=${TOKEN}`;
+  const jazzRequestUrl = `https://api.discogs.com/database/search?&format=album&style=jazz&type=release&country=US&per_page=75&year=2021&token=${TOKEN}`;
 
-module.exports = { logInUser, isUserLoggedIn };
+  const popAlbums = await cache.get(popRequestUrl, headers);
+  const hipHopAlbums = await cache.get(hipHopRequestUrl, headers);
+  const jazzAlbums = await cache.get(jazzRequestUrl, headers);
+  const allAlbums = [].concat(popAlbums.results, hipHopAlbums.results, jazzAlbums.results);
+  const matchingAlbum = allAlbums.filter(album => album.id === albumId);
+  
+  return matchingAlbum.length !== 0 ? matchingAlbum[0] : null;
+}
+
+
+module.exports = { logInUser, isUserLoggedIn, findMatchingAlbum };
